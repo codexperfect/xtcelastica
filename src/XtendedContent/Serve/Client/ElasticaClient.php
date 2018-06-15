0215327d97dcdc682d24a8ad3e867c7e8dd7e475
Alexandre Israël <bonjour@alexandre-israel.com>
2018-06-15 19:46:24 +0200

    options

--- src/XtendedContent/Serve/Client/ElasticaClient.php
+++ src/XtendedContent/Serve/Client/ElasticaClient.php
@@ -10,6 +10,8 @@ namespace Drupal\xtcelastica\XtendedContent\Serve\Client;
 
 
 use Drupal\xtc\XtendedContent\Serve\Client\AbstractClient;
+use Drupal\xtc\XtendedContent\Serve\Client\ClientInterface;
+use Elastica\Client;
 
 class ElasticaClient extends AbstractClient
 {
@@ -33,7 +35,7 @@ class ElasticaClient extends AbstractClient
    * @param string $method
    * @param string $param
    *
-   * @return \Drupal\xtc\XtendedContent\Serve\Client\ClientInterface
+   * @return ClientInterface
    */
   public function init($method, $param = '') : ClientInterface {
     $this->method = $method;
@@ -53,12 +55,37 @@ class ElasticaClient extends AbstractClient
   }
 
   /**
-   * @return \Drupal\xtc\XtendedContent\Serve\Client\ClientInterface
+   * @return ClientInterface
    */
   protected function buildClient() : ClientInterface {
     $this->setOptions();
-    $this->client = New
-    ($this->options);
+    $this->client = New Client($this->options);
+    return $this;
+  }
+
+  /**
+   * @return \Drupal\xtc\XtendedContent\Serve\Client\ClientInterface
+   */
+  public function setOptions()  : ClientInterface {
+    $this->setClientProfile();
+    $options = [
+      'host' => $this->getInfo('host'),
+      'port' => $this->getInfo('port'),
+      'path' => $this->getInfo('path'),
+      'url' => $this->getInfo('url'),
+      'proxy' => $this->getInfo('proxy'),
+      'transport' => $this->getInfo('transport'),
+      'persistent' => $this->getInfo('persistent'),
+      'timeout' => $this->getInfo('timeout'),
+      'connections' => $this->getInfo('connections'),
+      'roundRobin' => $this->getInfo('roundRobin'),
+      'log' => $this->getInfo('log'),
+      'retryOnConflict' => $this->getInfo('retryOnConflict'),
+      'bigintConversion' => $this->getInfo('bigintConversion'),
+      'username' => $this->getInfo('username'),
+      'password' => $this->getInfo('password'),
+    ];
+    $this->options = $options;
     return $this;
   }
 
