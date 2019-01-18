665e10bc684b954a8dd58a6f73dc0b988ed49e70
Alexandre Israël <bonjour@alexandre-israel.com>
2019-01-18 18:38:50 +0100

    cleanup

--- src/Form/XtcSearchForm.php
+++ src/Form/XtcSearchForm.php
@@ -2,32 +2,17 @@
 
 namespace Drupal\xtcelastica\Form;
 
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Url;
-use Drupal\xtc\Form\XtcSearchFormBase;
+use Drupal\xtcsearch\Form\XtcSearchFormBase;
 
 /**
- * Class AgendaForm.
+ * Class XtcSearchForm
+ *
+ * @package Drupal\xtcelastica\Form
  */
 class XtcSearchForm extends XtcSearchFormBase
 {
-  protected function getSearchId() {
+  public function getSearchId() {
     return 'search';
-  }
-
-  /**
-   * @return \Drupal\Core\GeneratedUrl|string
-   */
-  protected function resetLink(){
-    return Url::fromRoute('xtcelastica.xtcsearch')->toString();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $url = Url::fromRoute('xtcelastica.xtcsearch', ['s' => '*']);
-    $form_state->setRedirectUrl($url);
   }
 
 }
