32e4a30da143bcb31d4b8134a190bf96f2ef8e05
Alexandre Israël <bonjour@alexandre-israel.com>
2019-01-24 15:27:52 +0100

    init Elastica handler

---
+++ src/Plugin/XtcHandler/ElasticaBase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\xtcelastica\Plugin\XtcHandler;
+
+
+use Drupal\xtc\PluginManager\XtcHandler\XtcHandlerPluginBase;
+
+/**
+ * Plugin implementation of the xtc_handler for File.
+ *
+ */
+abstract class ElasticaBase extends XtcHandlerPluginBase
+{
+
+}
